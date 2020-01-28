0e5745f72af7ee5b6b2d9ee4100e1301be616c08
Jim Ntare <jim.ntare@Jims-MacBook-Pro-2.local>
2020-01-28 16:10:29 +0200

    feat: login into account
    create login controller
    use generatetoken file to help user access certain services
    add login POST to the authentiation routes
    
    [ Finishes #170868616 ]

--- server/api/routes/authRoutes.js
+++ server/api/routes/authRoutes.js
@@ -5,5 +5,6 @@ import UserController from '../controller/userController';
 const authRouter = express.Router();
 
 authRouter.post('/register', validateUser, UserController.registerAccount);
+authRouter.post('/login', UserController.login);
 
 export default authRouter;
